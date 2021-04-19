d2191cfcd5b42f0a4ec9d633c57d8bcb8c1a0231
Okuda, Jun'ichi <okudajun@amazon.com>
2021-04-19 21:02:46 +0000

    Rollback 368f358f and f4fb00db
    The script cask file for amazon-ssm-agent included in the commits
    are not compatible with Mojave and Catalina, and rolling back to
    the contents of b8c36b54.
    
    Signed-off-by: Matthew Carter <mattcat@amazon.com>
    Signed-off-by: Jon Oikawa <joikawa@amazon.com>

--- Casks/amazon-ssm-agent.rb
+++ Casks/amazon-ssm-agent.rb
@@ -13,55 +13,15 @@
 # limitations under the License.
 
 cask "amazon-ssm-agent" do
-  version "3.0.1031-0"
-  sha256 "d85c8ed6669802578a4484fc9f2ea4ddab04c7a1dd574bdbc3d61812eebf5970"
+  version "3.0.529-0"
+  sha256 "f09fa67e270f87e800de597d4317e6da0b81ce68e4f42b2ea16d7a565a31af8e"
 
   # amazon was verified as official when first introduced to the cask
-  url "https://aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ssm-agent/amazon-ssm-agent-#{version}.pkg"
+  url "https://aws-homebrew.s3-us-west-2.amazonaws.com/amazon-ssm-agent-3.0.529-0.pkg"
   name "Amazon SSM Agent"
   homepage "https://github.com/aws/amazon-ssm-agent"
 
-  script = Tempfile.new('ensure_no_com.amazon.aws.ssm_')
-  script.write <<~'EOS'
-    #! /bin/bash
-
-    num_trials=10
-
-    check_agent() {
-      launchctl list com.amazon.aws.ssm >/dev/null 2>&1
-    }
-
-    check_agent || exit 0
-
-    echo "Deleting the service system/com.amazon.aws.ssm"
-    if ! launchctl bootout system/com.amazon.aws.ssm; then
-        echo "Failed to delete system/com.amazon.aws.ssm"
-        exit 1
-    fi
-
-    n=0
-    while check_agent; do
-      if [ "$n" -ge "$num_trials" ]; then
-        echo "Waited for $num_trials seconds, but the agent is still running"
-        exit 1
-      fi
-      sleep 1
-      n="$((n+1))"
-    done
-    echo "system/com.amazon.aws.ssm has been deleted"
-  EOS
-  script.close
-
-  # This is for upgrading from old versions which don't have uninstall_preflight
-  preflight do
-    system_command "/bin/bash", args: [script.path], sudo: true
-  end
-
-  uninstall_preflight do
-    system_command "/bin/bash", args: [script.path], sudo: true
-  end
-
-  pkg "amazon-ssm-agent-#{version}.pkg"
+  pkg "amazon-ssm-agent-3.0.529-0.pkg"
 
   uninstall pkgutil: "com.amazon.aws.ssm"
 end
