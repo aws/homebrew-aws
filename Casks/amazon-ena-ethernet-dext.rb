fbaa29c15c45127acff2a972d0952a9d1d4568f2
Jacob Vallejo <jakeev@amazon.com>
2025-03-21 15:04:23 -0700

    refactor: restrict to supported installations

--- Casks/amazon-ena-ethernet-dext.rb
+++ Casks/amazon-ena-ethernet-dext.rb
@@ -1,11 +1,11 @@
 #   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,17 +16,15 @@ cask "amazon-ena-ethernet-dext" do
     version "1.0.7-p1"
     dextDriverKitVersion = "21.2"
 
-    if Hardware::CPU.arm?
-        if MacOS.version >= :monterey
-            url "https://aws-homebrew.s3.us-west-2.amazonaws.com/cask/amazon-ena-ethernet-dext/amazon-ena-ethernet-dext-app-#{version}-dk#{dextDriverKitVersion}-1.pkg",
-                verified: "amazon"
-            sha256 "464cb03d48ef97b032d7efe8af4662af256ef0a8cb292076782be674f00a16e7"
-            pkg "amazon-ena-ethernet-dext-app-#{version}-dk#{dextDriverKitVersion}-1.pkg"
-        else
-            raise "The amazon-ena-ethernet-dext Cask does not support macOS #{MacOS.version}."
-        end
-    else
-        raise "The amazon-ena-ethernet-dext Cask only supports the arm architecture."
+    depends_on macos: :monterey
+
+    url "https://aws-homebrew.s3.us-west-2.amazonaws.com/cask/amazon-ena-ethernet-dext/amazon-ena-ethernet-dext-app-#{version}-dk#{dextDriverKitVersion}-1.pkg",
+        verified: "amazon"
+    sha256 "464cb03d48ef97b032d7efe8af4662af256ef0a8cb292076782be674f00a16e7"
+    pkg "amazon-ena-ethernet-dext-app-#{version}-dk#{dextDriverKitVersion}-1.pkg"
+
+    on_intel do
+      disable! date: "2025-03-21", because: "unvalidated operating system major version"
     end
 
     name "Amazon ENA Ethernet Dext"
