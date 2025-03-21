8ac3ee96f76b0fcf21e3a3b025602f7e7e0445e6
Jacob Vallejo <jakeev@amazon.com>
2025-03-21 15:04:23 -0700

    refactor: restrict to supported installations

--- Casks/amazon-ena-ethernet.rb
+++ Casks/amazon-ena-ethernet.rb
@@ -1,11 +1,11 @@
 #   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,24 +17,26 @@ cask "amazon-ena-ethernet" do
 
   if MacOS.version <= :mojave
     url "https://aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ena-ethernet/amazon-ena-ethernet-#{version}.mojave.pkg",
-        verified: "amazon"
+        verified: "aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ena-ethernet/"
     sha256 "17220a622a37c49dd874c8cd0ff44a5223dbdc6fd9dbb22861b84d90abb79f3b"
     pkg "amazon-ena-ethernet-#{version}.mojave.pkg"
   elsif MacOS.version <= :catalina
     url "https://aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ena-ethernet/amazon-ena-ethernet-#{version}.catalina.pkg",
-        verified: "amazon"
+        verified: "aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ena-ethernet/"
     sha256 "183dfd41b56883d84d3fa79a724fb6613d598c6d74c8d8a292eb885b0ffa5167"
     pkg "amazon-ena-ethernet-#{version}.catalina.pkg"
   elsif MacOS.version <= :big_sur
     url "https://aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ena-ethernet/amazon-ena-ethernet-#{version}.bigsur.pkg",
-        verified: "amazon"
+        verified: "aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ena-ethernet/"
     sha256 "bbd9ab0382b306641598d101e7beec571c182d47ebd32efbae1f0e652aa0efa4"
     pkg "amazon-ena-ethernet-#{version}.bigsur.pkg"
   elsif MacOS.version <= :ventura
     url "https://aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ena-ethernet/amazon-ena-ethernet-#{version}.monterey.pkg",
-        verified: "amazon"
+        verified: "aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ena-ethernet/"
     sha256 "3cde67c25f339194753256ed911572bfa6a654b46b4af75d548dbcffc0b83634"
     pkg "amazon-ena-ethernet-#{version}.monterey.pkg"
+  else
+    disable! date: "2025-03-21", because: "unvalidated operating system major version"
   end
 
   name "Amazon ENA Ethernet"
