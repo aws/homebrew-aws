59726dbfea0365300f945c1837c010f2712a77dc
Abdala Aljewarane <aljew@amazon.com>
2026-09-15 11:39:45 -0700

    feat: update ec2-macos-init to 1.5.16

--- Casks/ec2-macos-init.rb
+++ Casks/ec2-macos-init.rb
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 cask "ec2-macos-init" do
-    version "1.5.15-2"
-    sha256 "70ac5413936b6e26fba55c4e0fa00cd47bf57339b2b4c631829e0a10835f7956"
+    version "1.5.16-2"
+    sha256 "61a2cf1731883e56123c90008755432a0a7dc7862e03c54859da4d7a41d6aef4"
 
     pkg_file = "ec2-macos-init-#{version}_universal.pkg"
 
